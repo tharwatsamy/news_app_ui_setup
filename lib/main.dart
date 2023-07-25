dbdd751e8bed6cc92a791860fefe05b20508bdf0
tharwatsamy <tharwatsamy2004@gmail.com>
2023-07-26 02:36:51 +0300

    response part two

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,11 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app_ui_setup/services/news_service.dart';
 import 'package:news_app_ui_setup/views/home_view.dart';
 
 void main() {
-  getGeneralNews();
+  NewsService(Dio()).getNews();
   runApp(const NewsApp());
-}
-
-final dio = Dio();
-
-void getGeneralNews() async {
-  final response = await dio.get(
-      'https://newsapi.org/v2/everything?q=bitcoin&apiKey=3c88955c487e4d9db668f011dd85e737');
-  print(response);
-}
-
-void getSportsNews() async {
-  final response = await dio.get(
-      'https://newsapi.org/v2/everything?q=bitcoin&apiKey=3c88955c487e4d9db668f011dd85e737');
-  print(response);
 }
 
 class NewsApp extends StatelessWidget {
--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -5,5 +5,12 @@ class NewsService {
 
   NewsService(this.dio);
 
-  getNews() {}
+  getNews() async {
+    var response = await dio.get(
+        'https://newsapi.org/v2/top-headlines?apiKey=3c88955c487e4d9db668f011dd85e737&country=eg&category=general');
+
+    Map<String, dynamic> jsonData = response.data;
+
+    List<dynamic> articles = jsonData['articles'];
+  }
 }
