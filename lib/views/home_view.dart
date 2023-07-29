b9d4a54d30213e803c4e7e11b132f787dbda58b3
tharwatsamy <tharwatsamy2004@gmail.com>
2023-07-29 03:16:38 +0300

    refactor news list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_app_ui_setup/widgets/categories_list_view.dart';
-import 'package:news_app_ui_setup/widgets/news_list_view.dart';
-import 'package:news_app_ui_setup/widgets/news_tile.dart';
+
+import '../widgets/news_list_view_builder.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -39,7 +39,7 @@ class HomeView extends StatelessWidget {
                 height: 32,
               ),
             ),
-            NewsListView(),
+            NewsListViewBuilder(),
           ],
         ),
         // child: Column(
---
+++ lib/widgets/news_list_view_builder.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import '../models/article_model.dart';
+import '../services/news_service.dart';
+import 'news_list_view.dart';
+
+class NewsListViewBuilder extends StatefulWidget {
+  const NewsListViewBuilder({
+    super.key,
+  });
+
+  @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  List<ArticleModel> articles = [];
+
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    getGeneralNews();
+  }
+
+  Future<void> getGeneralNews() async {
+    articles = await NewsService(Dio()).getNews();
+    isLoading = false;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? const SliverToBoxAdapter(
+            child: Center(child: CircularProgressIndicator()))
+        : NewsListView(
+            articles: articles,
+          );
+  }
+}
